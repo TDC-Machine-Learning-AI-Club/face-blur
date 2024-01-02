87475e351898d9fa30d55cb432182b859e171dd5
Mohamed Singano <singano2009@gmail.com>
2024-01-02 17:58:46 +0300

    added remote upload download

---
+++ api/helpers/uploader.py
@@ -0,0 +1,28 @@
+import os
+from supabase import create_client, Client
+import logging
+import sys
+import mimetypes
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+STORAGE_URL = os.environ.get('STORAGE_URL')
+SUPABASE_API_KEY = os.environ.get('SUPABASE_API_KEY')
+def get_mime_type(image_path):
+    mime_type, _ = mimetypes.guess_type(image_path)
+    return mime_type
+supabase: Client = create_client(STORAGE_URL, SUPABASE_API_KEY)
+
+def upload_image_files(filepath):
+    content_type = get_mime_type(filepath)
+    logging.disable(sys.maxsize)
+    file_name = filepath.rsplit('/', 1)[-1]
+    with open(filepath, 'rb') as f:
+        supabase.storage.from_("imagesbucket").upload(file=f,path=file_name, file_options={"content-type": content_type})
+        
+        return supabase.storage.from_('imagesbucket').get_public_url(file_name)
+    
+        #this shall return a url just like this one https://hriwdlfljbreqtaymgfn.supabase.co/storage/v1/object/public/imagesbucket/DALL-E-AI-girl.png?
+
+#print(upload_image_files('../images/input/DALL-E-AI-girl.png'))
+
--- api/test.py
+++ api/test.py
@@ -7,4 +7,12 @@ def get_file_extension(file_path):
 # Example usage:
 file_path = "images/DALL-E-girl.png"
 extension = get_file_extension(file_path)
-print(extension)
+#print(extension)
+
+import mimetypes
+
+def get_mime_type(image_path):
+    mime_type, _ = mimetypes.guess_type(image_path)
+    return mime_type
+
+print(get_mime_type(file_path))
