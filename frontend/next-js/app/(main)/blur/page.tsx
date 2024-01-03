787533c203b97edcb44e37796a6477dbe5d82ad5
Fredy <fredgerman30@gmail.com>
2024-01-03 21:45:14 +0300

    feat : home page wip

---
+++ frontend/next-js/app/(main)/blur/page.tsx
@@ -0,0 +1,14 @@
+import FileUploadForm from "@/components/forms/image_upload_form";
+
+export default function IndexPage() {
+  return (
+    <div className="prose prose-sm prose-invert my-12 text-foreground ">
+      <h1 className="text-4xl font-bold text-foreground ">
+        Upload images you want to
+        {""}
+        <span className=" text-foreground/50"> Blur</span>
+      </h1>
+      <FileUploadForm />
+    </div>
+  );
+}
